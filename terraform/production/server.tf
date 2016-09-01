473f84a5dc004a77bfd6d689477460fce0efea8d
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-09-02 01:38:21 +0200

    using cloudflare

--- terraform/production/server.tf
+++ terraform/production/server.tf
@@ -11,23 +11,32 @@ resource "digitalocean_droplet" "app1" {
   ssh_keys = ["${var.ssh_fingerprint}"]
 }
 
-resource "digitalocean_record" "www" {
+# Configure the CloudFlare provider
+provider "cloudflare" {
+  email = "${var.cloudflare_email}"
+  token = "${var.cloudflare_token}"
+}
+
+resource "cloudflare_record" "www" {
     domain = "my-grocery-price-book.co.za"
     type = "A"
     name = "www"
     value = "${digitalocean_droplet.app1.ipv4_address}"
+    proxied = true
 }
 
-resource "digitalocean_record" "app1" {
+resource "cloudflare_record" "app1" {
   domain = "my-grocery-price-book.co.za"
   type = "A"
   name = "app1"
   value = "${digitalocean_droplet.app1.ipv4_address}"
+  proxied = false
 }
 
-resource "digitalocean_record" "db" {
+resource "cloudflare_record" "db" {
   domain = "my-grocery-price-book.co.za"
   type = "A"
   name = "db"
   value = "178.62.104.169"
+  proxied = false
 }
--- terraform/production/vars.tf
+++ terraform/production/vars.tf
@@ -1,2 +1,4 @@
 variable "do_api_token" {}
 variable "ssh_fingerprint" {}
+variable "cloudflare_email" {}
+variable "cloudflare_token" {}
