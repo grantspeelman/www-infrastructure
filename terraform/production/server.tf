d7615d63f098d03f3ca987bf7c8880e55e7cfdd0
Grant Petersen-Speelman <grantspeelman@users.noreply.github.com>
2017-03-24 23:05:28 +0200

    Remove old servers

--- terraform/production/server.tf
+++ terraform/production/server.tf
@@ -3,30 +3,6 @@ provider "digitalocean" {
   token = "${var.do_api_token}"
 }
 
-resource "digitalocean_droplet" "app-runner1" {
-  image = "ubuntu-16-04-x64"
-  name = "my-grocery-book-app-runner1"
-  region = "lon1"
-  size = "512mb"
-  ssh_keys = ["${var.ssh_fingerprint}"]
-}
-
-resource "digitalocean_droplet" "app-runner2" {
-  image = "ubuntu-16-04-x64"
-  name = "my-grocery-book-app-runner2"
-  region = "lon1"
-  size = "512mb"
-  ssh_keys = ["${var.ssh_fingerprint}"]
-}
-
-resource "digitalocean_droplet" "web-balancer" {
-  image = "ubuntu-16-04-x64"
-  name = "my-grocery-book-web-balancer"
-  region = "lon1"
-  size = "1gb"
-  ssh_keys = ["${var.ssh_fingerprint}"]
-}
-
 # Configure the CloudFlare provider
 provider "cloudflare" {
   email = "${var.cloudflare_email}"
@@ -39,30 +15,6 @@ resource "cloudflare_record" "www" {
     name = "www"
     value = "139.59.184.144"
     proxied = true
-}
-
-resource "cloudflare_record" "web-balancer" {
-  domain = "my-grocery-price-book.co.za"
-  type = "A"
-  name = "web-balancer"
-  value = "${digitalocean_droplet.web-balancer.ipv4_address}"
-  proxied = false
-}
-
-resource "cloudflare_record" "app-runner1" {
-  domain = "my-grocery-price-book.co.za"
-  type = "A"
-  name = "app-runner1"
-  value = "${digitalocean_droplet.app-runner1.ipv4_address}"
-  proxied = false
-}
-
-resource "cloudflare_record" "app-runner2" {
-  domain = "my-grocery-price-book.co.za"
-  type = "A"
-  name = "app-runner2"
-  value = "${digitalocean_droplet.app-runner2.ipv4_address}"
-  proxied = false
 }
 
 resource "cloudflare_record" "db" {
